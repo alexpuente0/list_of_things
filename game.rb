e21bb0f3339a858529203691bd039b97a3b4c16a
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-02 00:53:11 +0300

    Fix: linter issues

--- game.rb
+++ game.rb
@@ -1,17 +1,18 @@
 require_relative 'item'
-class Game << Item
-    attr_accessor :multiplayer,:last_played_at
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
   def initialize(genre, author, label, publish_date,
-                multiplayer,last_played_at,archived)
-                super(publish_date,archived: archived)
-    @genre=genre
-    @author=author
-    @label=label
-    @multiplayer=multiplayer
-    @last_played_at=last_played_at
+                 multiplayer, last_played_at, archived)
+    super(publish_date, archived: archived)
+    @genre = genre
+    @author = author
+    @label = label
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
   end
+
   private
 
-  def can_be_archived?
-  end
+  def can_be_archived?; end
 end
