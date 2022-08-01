062c4681fab652cd37395c19d3283a1e424f00d1
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-02 01:46:50 +0300

    test: game class

---
+++ spec/game_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../game'
+require_relative '../author'
+describe Author do
+  context "check for data type and value" do
+    before (:each) do
+       @author=Author.new("Tadesse", "Alemayehu")
+      @game=Game.new(nil, @author, nil, "2000-12-05",
+                 2, "2015-12-05", false)
+    end
+    it "assert author is instance of Author" do
+      expect(@game.author).to be_an_instance_of(Author)
+    end
+    it "assert game property have the right value" do
+      expect(@game.author.first_name).to eq("Tadesse")
+      expect(@game.author.last_name).to eq("Alemayehu")
+    end
+  end
+end
