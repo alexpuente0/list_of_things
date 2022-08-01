60d66cc549792009068138dc1fa24ec49c005c11
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-01 22:35:05 +0300

    Define: constructor

--- item.rb
+++ item.rb
@@ -1,2 +1,8 @@
 class Item
+  attr_accessor :genre,:author,:source,:label,:publish_date
+  def initialize(publish_date,archived: false)
+    @id=rand(0..100000)
+    @publish_date=Date.parse(publish_date)
+  end
+
 end
