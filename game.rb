ed5be0a52697289ea0a4a2f61eb9ba1a1f09f25b
Tadesse-Alemayehu <email24t@gmail.com>
2022-08-02 00:45:56 +0300

    etup: Game Class

--- game.rb
+++ game.rb
@@ -1,2 +1,17 @@
-class Game
+require_relative 'item'
+class Game << Item
+    attr_accessor :multiplayer,:last_played_at
+  def initialize(genre, author, label, publish_date,
+                multiplayer,last_played_at,archived)
+                super(publish_date,archived: archived)
+    @genre=genre
+    @author=author
+    @label=label
+    @multiplayer=multiplayer
+    @last_played_at=last_played_at
+  end
+  private
+
+  def can_be_archived?
+  end
 end
