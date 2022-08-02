02a77108118f182fffa3433d5e3dba6289f0b360
arturo <ahpareja@gmail.com>
2022-08-02 11:26:25 -0500

    Add add_item method on genre class

--- genre.rb
+++ genre.rb
@@ -1,7 +1,21 @@
 class Genre
+  attr_accessor :name
+
+  @@genres = []
+
   def initialize(name)
     @id = rand(0..100_000)
     @name = name
     @items = []
+    @@genres << self
+  end
+
+  def add_item(item)
+    @items << item unless @items.include? item
+    item.genre = self
+  end
+
+  def self.genres
+    @@genres
   end
 end
