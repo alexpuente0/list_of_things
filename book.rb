c4328b6c78680c9e8dba1985799b256fb2528d27
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-08-02 13:29:38 -0500

    Fix Linters

--- book.rb
+++ book.rb
@@ -1,11 +1,11 @@
 require_relative './item'
 
 class Book < Item
-attr_accessor :publisher, :cover_state, :archived
+  attr_accessor :publisher, :cover_state, :archived
 
-@@books= []
+  @@books = []
 
-  def initialize(genre, author, label, publish_date, publisher, cover_state, archived: false)
+  def initialize(genre, author, label, publish_date, publisher, cover_state)
     super(publish_date)
     @genre = genre
     @author = author
@@ -15,34 +15,32 @@ attr_accessor :publisher, :cover_state, :archived
     @@books << self
   end
 
-    def self.books
+  def self.books
     @@books
   end
+
   private
 
   def can_be_archived?
-    return true if super == true || @cover_state == 'bad'
-
-    false
+    return true if super == true or @cover_state == 'bad'
   end
 end
 
-  def list_all_books
-    Book.books.each do |book|
-      print "
+def list_all_books
+  Book.books.each do |book|
+    print "
       publish_date: #{book.publish_date}
       archived: #{book.archived}
       publisher : #{book.publisher}
       cover_state: #{book.cover_state}
       "
-    end
   end
+end
 
+# test_a = Book.new('genGenre', 'genAuthor', 'genLabel', '24/12/2020', 'genPublisher', 'bad')
+# test_b = Book.new('erhga', 'eddd', 'hhhhhhh', '24/12/2020', 'genPublisher', 'new')
+# test_c = Book.new('tteht', 'gtggg', 'XXX', '23/12/2020', 'genPublisher', 'bad')
+# test_d = Book.new('3+36', 'hhvvh', 'rererere', '22/12/2000', 'genPublisher', 'missing')
+# test_e = Book.new('trwhstrb', 'erererr', 'qqqq', '21/12/2004', 'genPublisher', 'bad')
 
-test1 = Book.new('genGenre','genAuthor','genLabel','24/12/2020','genPublisher','bad')
-test2 = Book.new('erhga','eddd','hhhhhhh','24/12/2020','genPublisher','new')
-test3 = Book.new('tteht','gtggg','XXX','23/12/2020','genPublisher','bad')
-test4 = Book.new('3+36','hhvvh','rererere','22/12/2000','genPublisher','missing')
-test5 = Book.new('trwhstrb','erererr','qqqq','21/12/2004','genPublisher','bad')
-
-list_all_books()
+# list_all_books
--- label.rb
+++ label.rb
@@ -1,7 +1,7 @@
 class Label
   attr_accessor :title, :color, :id
 
-  def initialize(title, color, id)
+  def initialize(title, color, _id)
     @id = rand(0..100_000)
     @title = title
     @color = color
@@ -16,9 +16,9 @@ class Label
   end
 end
 
-test01 = Label.new('generic title','red', 9)
+# test01 = Label.new('generic title', 'red', 9)
 
-p test01
-p test01.title
-p test01.color
-p test01.id
+# p test01
+# p test01.title
+# p test01.color
+# p test01.id
