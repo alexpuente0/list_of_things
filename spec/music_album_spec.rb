98f6c8b87a1d87873cf6b8484f4a59af53157b7d
arturo <ahpareja@gmail.com>
2022-08-02 17:19:41 -0500

    bug fix

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -2,15 +2,15 @@ require_relative '../music_album'
 require_relative '../genre'
 require 'date'
 
-describe MusicAlbun do
+describe MusicAlbum do
   before :each do
     @genre = Genre.new('Rock')
-    @album = MusicAlbun.new(@genre, 'Bon Jovi', 'label', '2018-12-05', false, false)
+    @album = MusicAlbum.new(@genre, 'Bon Jovi', 'label', '2018-12-05', false, false)
   end
 
   describe 'music' do
     it 'Should return a music object' do
-      expect(@album).to be_an_instance_of(MusicAlbun)
+      expect(@album).to be_an_instance_of(MusicAlbum)
     end
     it 'Should have' do
       expect(@album.genre.name).to eq('Rock')
