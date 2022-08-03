3dcec9fde61fb43413b153dfdefacd61470c920d
arturo <ahpareja@gmail.com>
2022-08-03 09:24:20 -0500

    add preserve data to genre class

--- genre.rb
+++ genre.rb
@@ -1,13 +1,26 @@
+require 'json'
 class Genre
   attr_accessor :name, :items
+  attr_reader :id
 
   @@genres = []
 
-  def initialize(name)
-    @id = rand(0..100_000)
+  def initialize(name, id: nil)
+    @id ||= rand(0..100_000)
     @name = name
     @items = []
     @@genres << self
+  end
+
+  def self.save_albums
+    json_array=[]
+    @@genres.each do |e|
+      json_array << [ e.id, e.name]
+    end
+    p json_array
+    genre_db=File.new('genre.json','g')
+    genre_db.write(JSON.generate(json_array))
+    genre_db.close
   end
 
   def add_item(item)
@@ -18,9 +31,19 @@ class Genre
     @@genres
   end
 
+  private
+
   def list_all_genres
     Genre.genres.each do |genre|
-      puts genre
+      puts `id: #{genre.id}
+            gengre: #{genre.name}`
     end
   end
 end
+
+def save_genres
+  Genre.save_genres
+end
+
+list_all_genres
+save_genres
