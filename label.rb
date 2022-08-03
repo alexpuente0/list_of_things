a520793c365895de69f7a9e7d3ce0c7861d06eb9
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-08-03 11:57:04 -0500

    Data preservation for Label Module

--- label.rb
+++ label.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Label
   attr_accessor :title, :color
   attr_reader :id, :items
@@ -22,6 +24,16 @@ class Label
   end
 end
 
+  def self.save_labels
+    json_array = []
+    Label.labels.each do |lb|
+      json_array << { title: lb.title, color: lb.color }
+    end
+    label_db = File.new('label.json', 'w')
+    label_db.write(JSON.generate(json_array))
+    label_db.close
+  end
+
 def list_all_labels
   Label.labels.each do |label|
     print "
@@ -36,3 +48,28 @@ end
 
 # list_all_labels
 
+def add_label
+  puts 'Insert title: '
+  title = gets.chomp
+  puts 'Insert color: '
+  color = gets.chomp
+  Label.new(title, color)
+end
+
+def save_labels
+  Label.save_labels
+end
+
+def load_labels
+  return [] unless File.exist?('./label.json')
+
+  file = File.open('./label.json')
+  read_file = File.read(file)
+  read_json = JSON.parse(read_file)
+
+  loaded_labels = []
+
+  read_json.each do |label|
+    loaded_labels.push(Label.new(label['title'], label['color']))
+  end
+end
