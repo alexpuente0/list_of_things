c132aac8940fdd31f2edd0a4dc2b53c41cc03157
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-08-03 11:39:44 -0500

    Book Data Preservation

--- book.rb
+++ book.rb
@@ -1,3 +1,4 @@
+require 'json'
 require_relative './item'
 
 class Book < Item
@@ -19,6 +20,22 @@ class Book < Item
     @@books
   end
 
+  def self.books(item)
+    @@books = item
+  end
+
+  def self.save_books
+    json_array = []
+    Book.books.each do |bk|
+      json_array << { genre: bk.genre, author: bk.author,
+                      label: bk.label, publish_dt: bk.publish_date,
+                      publisher: bk.publisher, cover_st: bk.cover_state }
+    end
+    book_db = File.new('book.json', 'w')
+    book_db.write(JSON.generate(json_array))
+    book_db.close
+  end
+
   private
 
   def can_be_archived?
@@ -33,25 +50,46 @@ def list_all_books
       archived: #{book.archived}
       publisher: #{book.publisher}
       cover_state: #{book.cover_state}
-      label: #{book.label}
       "
   end
 end
 
 def add_book(genre, author, label)
-    puts 'Insert published date: '
-      publish_date = gets.chomp
-    puts 'Insert publisher: '
-      publisher = gets.chomp
-    puts 'Insert cover state:  '
-      cover_state = gets.chomp
-    Book.new(genre, author, label,
-                   publish_date, publisher, cover_state)
+  puts 'Insert published date: '
+  publish_date = gets.chomp
+  puts 'Insert publisher: '
+  publisher = gets.chomp
+  puts 'Insert cover state:  '
+  cover_state = gets.chomp
+  Book.new(genre, author, label,
+           publish_date, publisher, cover_state)
 end
 
-# add_book('scyfi','tolkien','red')
+def save_books
+  Book.save_books
+end
 
-#  test_a = Book.new('genGenre', 'genAuthor', 'ddd', '24/12/2000', 'genPublisher', 'bad')
-#  test_b = Book.new('erhga', 'eddd', 'hhhhhhh', '24/12/2000', 'genPublisher', 'good')
+def load_books
+  return [] unless File.exist?('./book.json')
+
+  file = File.open('./book.json')
+  read_file = File.read(file)
+  read_json = JSON.parse(read_file)
+
+  loaded_books = []
+
+  read_json.each do |book|
+    loaded_books.push(Book.new(book['genre'], book['author'], book['label'], book['publish_dt'], book['publisher'],
+                               book['cover_st']))
+  end
+
+  file.close
+  Book.books(loaded_books)
+end
+
+# add_book('scyfi', 'tolkien', 'red')
+
+# test_a = Book.new('genGenre', 'genAuthor', 'ddd', '24/12/2000', 'genPublisher', 'bad')
+# test_b = Book.new('erhga', 'eddd', 'hhhhhhh', '24/12/2000', 'genPublisher', 'good')
 
 # list_all_books
