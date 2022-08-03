7f9687dc5428ae62dc7e3a62e28d8953962e3765
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-08-03 12:29:43 -0500

    Create table on schema.sql

--- schema.sql
+++ schema.sql
@@ -19,3 +19,41 @@ CREATE TABLE game(
   CONSTRAINT author_fk FOREIGN KEY(author) REFERENCES author(id),
   CONSTRAINT label_fk FOREIGN KEY(label) REFERENCES label(id)
   );
+
+  CREATE TABLE genre(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name  TEXT NOT NULL,
+  )
+
+CREATE TABLE music_album(
+  id INT GENERATED ALWAYS IDENTITY PRIMARY KEY,
+  gener INT,
+  author INT,
+  label INT,
+  publish_date DATE,
+  archived BOOLEAN,
+  on_soptify BOOLEAN,
+  CONSTRAINT gener_fk FOREIGN KEY(gener) REFERENCES gener(id),
+  CONSTRAINT author_fk FOREIGN KEY(author) REFERENCES author(id),
+  CONSTRAINT label_fk FOREIGN KEY(label) REFERENCES label(id)
+  );
+
+CREATE TABLE label(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title TEXT,
+  color TEXT,
+);
+
+CREATE TABLE books(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  genre_id INT,
+  author_id INT,
+  label_id INT,
+  publish_date DATE,
+  publisher TEXT,
+  cover_state TEXT,
+  archived BOOLEAN DEFAULT false,
+  CONSTRAINT genre_fk FOREIGN KEY(genre) REFERENCES genre(id),
+  CONSTRAINT author_fk FOREIGN KEY(author) REFERENCES author(id),
+  CONSTRAINT label_fk FOREIGN KEY(label) REFERENCES label(id)
+);
